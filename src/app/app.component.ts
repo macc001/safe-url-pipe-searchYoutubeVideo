e3aa3f1754f0827867d407e50ca1273139cd5b5c
macc001 <miguelcayocarvajal37@gmail.com>
2022-06-20 17:39:19 -0400

    Performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,29 @@
-import { Component } from "@angular/core";
-import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  safeUrl: SafeResourceUrl;
-  results$: Observable<Array<any>>;
+  idVideos = [
+    {
+      id: 'https://www.youtube.com/embed/xtp_DuPxo9Q',
+      title: 'TypeScript - Tutorial desde CERO en Español ',
+    },
+    {
+      id: 'https://www.youtube.com/embed/KknN6-HKhiE',
+      title:
+        'Recomendaciones para ser Freelance en el mundo de la programación',
+    },
+    {
+      id: 'https://www.youtube.com/embed/d9y0l7yY404',
+      title:
+        'Mejorando el Powershell de Windows con OhmyPosh, Windows Terminal y NerdFonts',
+    },
+  ];
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private http: HttpClient
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.results$ = this.http.get(
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=robocop&topicId=%2Fm%2F02vxn&key=AIzaSyB42WhSTkS6_0uUPX6EuGakkGz4RHXnlIc"
-    ).pipe(
-      map(res => res.items),
-      map((items: Array<any>) => {
-        return items.map(item => ({
-          title: item.snippet.title,
-          vidoeUrl: `https://www.youtube.com/embed/${item.id.videoId}`,
-        }))
-      })
-    );
-  }
+  ngOnInit() {}
 }
